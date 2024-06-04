419677f52fa076fdd00545864300628a8e247b11
tienyasuo0729 <tienngqueson@gmail.com>
2024-06-04 23:42:15 +0700

    code

--- Shopee project/Database/shopee.sql
+++ Shopee project/Database/shopee.sql
@@ -3,82 +3,77 @@ USE Fake_shopee;
 
 create table area (
 	area_id int primary key,
-    name_area varchar(200)
-);
+    name_area varchar(200) not null	
+);    
 
 CREATE TABLE main_account (
-    main_account_id INT AUTO_INCREMENT PRIMARY KEY,
-	business_ID varchar(500),
-    password varchar(99),
-    account_area int,
-    phone_number varchar(14),
-    email varchar(500),
-	FOREIGN KEY (account_area) REFERENCES area(area_id)
-);
-    
-
-CREATE TABLE list_email_to_receive_electronic_invoices (
-    email_receive_electronic_id INT AUTO_INCREMENT PRIMARY KEY,
-    email_address VARCHAR(500 ) NOT NULL,
-    main_account_id INT,
-    FOREIGN KEY (main_account_id) REFERENCES main_account(main_account_id)
-);
-
-CREATE TABLE partner_platform (
-    platform_id INT AUTO_INCREMENT PRIMARY KEY,
-    main_account_id INT,
-    FOREIGN KEY (main_account_id) REFERENCES main_account(main_account_id)
-);
-
-CREATE TABLE payment_settings (
-    payment_settings_id INT AUTO_INCREMENT PRIMARY KEY,
-    automatic_withdrawal boolean,
-    pin_code VARCHAR(255),
-    main_account_id INT,
-    FOREIGN KEY (main_account_id) REFERENCES main_account(main_account_id)
-);
-
-CREATE TABLE chat_settings (
-    chat_settings_id INT AUTO_INCREMENT PRIMARY KEY,
-    Receive_messages_from_Shopee_Rewards BOOLEAN,
-    Receive_messages_from_Personal_Page BOOLEAN,
-    Play_sound_notification_for_new_messages BOOLEAN,
-    Push_new_popup_message BOOLEAN,
-    main_account_id INT,
-    FOREIGN KEY (main_account_id) REFERENCES main_account(main_account_id)
-);
-
-CREATE TABLE notification_settings (
-    notification_settings_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_update_notification boolean,
-    Newsletter_notification boolean,
-    Product_Update_Notification boolean,
-    Personal_Content_Notification boolean,
-    Chat_Messages_Reminder boolean,
-    main_account_id INT,
-    FOREIGN KEY (main_account_id) REFERENCES main_account(main_account_id)
+    id_main_account INT AUTO_INCREMENT PRIMARY KEY,
+	business_ID varchar(500) not null, -- đại diện cho tên đăng nhập và khi đăng nhập sẽ kèm theo :main gì đó phía sau có lẽ để phân biệt quyền gì đó
+    password varchar(99) not null,
+    account_area int not null, -- đại diện cho đất nước người này quản lý chính
+    phone_number varchar(14) not null,
+    email varchar(500) not null,
+	FOREIGN KEY (account_area) REFERENCES area(area_id) 
 );
 
 CREATE TABLE shop (
-    shop_id INT AUTO_INCREMENT PRIMARY KEY,
+    id_shop INT AUTO_INCREMENT PRIMARY KEY,
     phone_number VARCHAR(15) NOT NULL,
-    shop_name VARCHAR(100) NOT NULL,
-	email VARCHAR(500 ) NOT NULL,
 	password VARCHAR(99) NOT NULL,
-	shop_avatar_image VARCHAR(500 ) NOT NULL,
-	status boolean default 0 not null ,
-    main_account_id INT,
-    FOREIGN KEY (main_account_id) REFERENCES main_account(main_account_id)
+    email VARCHAR(500) NOT NULL,
+    shop_name VARCHAR(20) NOT NULL,
+	shop_avatar_image VARCHAR(500) null,
+	status boolean default 0 not null,
+    id_main_account INT null,
+    FOREIGN KEY (id_main_account) REFERENCES main_account(id_main_account) ON DELETE SET NULL
+);
+
+CREATE TABLE list_email_to_receive_electronic_invoices (
+    id_email_receive_electronic INT PRIMARY KEY AUTO_INCREMENT,
+    email_address VARCHAR(500) NOT NULL unique,
+    id_shop INT not null,
+    FOREIGN KEY (id_shop) REFERENCES shop(id_shop) ON DELETE CASCADE
+);
+
+CREATE TABLE partner_platform (
+    id_platform INT AUTO_INCREMENT PRIMARY KEY,
+    shop_id INT not null,
+    FOREIGN KEY (shop_id) REFERENCES shop(shop_id) ON DELETE CASCADE
+);
+
+CREATE TABLE payment_settings (
+    shop_id INT PRIMARY KEY,
+    automatic_withdrawal boolean default 0 not null,
+    pin_code VARCHAR(255) null,
+    FOREIGN KEY (shop_id) REFERENCES shop(shop_id) ON DELETE CASCADE
+);
+
+CREATE TABLE chat_settings (
+    shop_id INT PRIMARY KEY,
+    Receive_messages_from_Shopee_Rewards BOOLEAN default 0 not null,
+    Receive_messages_from_Personal_Page BOOLEAN default 0 not null,
+    Play_sound_notification_for_new_messages BOOLEAN default 0 not null,
+    Push_new_popup_message BOOLEAN default 0 not null,
+    FOREIGN KEY (shop_id) REFERENCES shop(shop_id) ON DELETE CASCADE
+);
+
+CREATE TABLE notification_settings (
+    shop_id INT PRIMARY KEY,
+    order_update_notification boolean default 0 not null,
+    Newsletter_notification boolean default 0 not null,
+    Product_Update_Notification boolean default 0 not null,
+    Personal_Content_Notification boolean default 0 not null,
+    Chat_Messages_Reminder boolean default 0 not null,
+    FOREIGN KEY (shop_id) REFERENCES shop(shop_id) ON DELETE CASCADE
 );
 
 CREATE TABLE business_information (
-    business_id INT AUTO_INCREMENT PRIMARY KEY,
+    shop_id INT PRIMARY KEY,
     business_type VARCHAR(100) NOT NULL,
     address_to_take_product VARCHAR(100) NOT NULL,
     registered_business_address  VARCHAR(100) NOT NULL,
-    tax_identìication_number VARCHAR(50),
-    shop_id INT,
-    FOREIGN KEY (shop_id) REFERENCES shop(shop_id)
+    tax_identìication_number VARCHAR(50) null,
+    FOREIGN KEY (shop_id) REFERENCES shop(shop_id) ON DELETE CASCADE
 );
 
 create table form_of_identification (
@@ -87,27 +82,27 @@ create table form_of_identification (
 );
 
 CREATE TABLE identification_shop (
-    identification_shop_id INT AUTO_INCREMENT PRIMARY KEY,
+    shgop_id INT PRIMARY KEY,
     form_of_identification int not null,
     identification_number varchar(500) not null,
     identification_name varchar(500) not null,
     image_identification_front varchar(500) not null,
     image_identification_back varchar(500) not null,
-    shgop_id INT,
-    FOREIGN KEY (shop_id) REFERENCES shop(shop_id)
+    FOREIGN KEY (form_of_identification) REFERENCES form_of_identification(form_id),
+    FOREIGN KEY (shop_id) REFERENCES shop(shop_id) ON DELETE CASCADE
 );
 
 CREATE TABLE shipping_method (
-    shipping_method_id INT AUTO_INCREMENT PRIMARY KEY,
-    method_name VARCHAR(100)
+    id_shipping_method INT AUTO_INCREMENT PRIMARY KEY,
+    method_name VARCHAR(100) not null
 );
 
 CREATE TABLE shop_shipping_method (
-    shop_shipping_method_id INT AUTO_INCREMENT PRIMARY KEY,
-    shop_id INT,
-    shipping_method_id INT,
-    FOREIGN KEY (shop_id) REFERENCES shop(shop_id),
-    FOREIGN KEY (shipping_method_id) REFERENCES shipping_method(shipping_method_id)
+    id_shop_shipping_method INT AUTO_INCREMENT PRIMARY KEY,
+    id_shop INT,
+    id_shipping_method INT,
+    FOREIGN KEY (id_shop) REFERENCES shop(id_shop) ON DELETE CASCADE,
+    FOREIGN KEY (id_shipping_method) REFERENCES shipping_method(id_shipping_method) ON DELETE CASCADE
 );
 
 CREATE TABLE product_group (
@@ -126,7 +121,7 @@ CREATE TABLE product_shipping_method (
 );
 
 CREATE TABLE user (
-    id INT PRIMARY KEY,
+    id_user INT PRIMARY KEY auto_increment,
     phone_number  VARCHAR(15) NOT NULL,
     password  VARCHAR(99) NOT NULL,
     account_name varchar(500) not null,
