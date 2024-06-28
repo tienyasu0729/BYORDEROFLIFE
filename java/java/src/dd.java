ad141a2d4ffac924f63ae911164e0e1aab9ac830
tienyasuo0729 <tienngqueson@gmail.com>
2024-06-28 16:13:33 +0700

    code

--- java/java/src/dd.java
+++ java/java/src/dd.java
@@ -1,12 +1,9 @@
 public class dd {
     public static void main(String[] args) {
-        int i = 10;
-        Object o = i;
-        o = 100;
-        System.out.println(o);
-        System.out.println(i);
-        i = 99;
-        System.out.println(o);
-        System.out.println(i);
+        int i=1;
+        int n = 29;
+        switch (i){
+
+        }
     }
 }
