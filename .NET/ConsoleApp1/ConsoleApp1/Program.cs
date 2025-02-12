bc9a2379171861802f84ce1d2b4f522bc0b3816c
tienyasu0729 <tienngqueson@gmail.com>
2025-02-12 16:06:05 +0700

    code

---
+++ .NET/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        var name = Console.ReadLine();
+        
+    }
+}
---
+++ .NET/ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] test = new[] { 3, 5, 1, 0 };
+        Console.WriteLine("Hello World! Array lenght: " + test.Length);
+        
+    }
+}
