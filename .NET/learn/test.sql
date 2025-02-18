412264ac682a38370694f08a92a8d64668c6bef9
tienyasu0729 <tienngqueson02@gmail.com>
2025-02-18 16:54:10 +0700

    code

---
+++ .NET/learn/test.sql
@@ -0,0 +1,24 @@
+create database Employee;
+
+create table SalariedEmployee(
+	SSN varchar(500) primary key,
+	FirstName varchar(500),
+    LastName varchar(500),
+    BirthDate datetime,
+    Phone varchar(500),
+    Email varchar(500),
+    CommissionRate double,
+    GrossSales double,
+    BasicSalary double
+);
+
+create table HourlyEmployee(
+	SSN varchar(500) primary key,
+	FirstName varchar(500),
+    LastName varchar(500),
+    BirthDate datetime,
+    Phone varchar(500),
+    Email varchar(500),
+    Wage double,
+    WorkingHours double	
+);
