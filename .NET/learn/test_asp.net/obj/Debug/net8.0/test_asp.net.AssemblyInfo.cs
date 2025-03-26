ecfc0bd86f829b00c4320561e6d243e2aebdffcf
tienyasu0729 <tienngqueson02@gmail.com>
2025-03-26 16:52:32 +0700

    code

--- .NET/learn/test_asp.net/obj/Debug/net8.0/test_asp.net.AssemblyInfo.cs
+++ .NET/learn/test_asp.net/obj/Debug/net8.0/test_asp.net.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test_asp.net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f876b05544ac94a005a90b1304390bfffbd9d041")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fb1a995e6b7934bb4ad76c7e232c38ee8f03e55")]
 [assembly: System.Reflection.AssemblyProductAttribute("test_asp.net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test_asp.net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
