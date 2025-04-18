7f21d811542581dd42f1dfdef23975e71884144a
tienyasuo0729 <tienngqueson@gmail.com>
2025-04-18 11:11:50 +0700

    code

--- .NET/Project learn/test_Dbcontext_asp/test_Dbcontext_asp/obj/Debug/net8.0/test_Dbcontext_asp.AssemblyInfo.cs
+++ .NET/Project learn/test_Dbcontext_asp/test_Dbcontext_asp/obj/Debug/net8.0/test_Dbcontext_asp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test_Dbcontext_asp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1662f96d6057f80661904758ebf4d494eb0c74e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c1ffb8c04a9c866f96d7ac5b9413c27d7341a05")]
 [assembly: System.Reflection.AssemblyProductAttribute("test_Dbcontext_asp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test_Dbcontext_asp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
